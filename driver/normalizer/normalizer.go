0712fe05146f52579a6f3fdab09cb69f85091ac1
Juanjo Alvarez <juanjux@gmail.com>
2017-03-14 12:24:33 +0100

    Adapted to the new SDK (childrens pending)

--- driver/normalizer/normalizer.go
+++ driver/normalizer/normalizer.go
@@ -1,12 +1,14 @@
 package normalizer
 
+// FIXME: change "juanjux" to bblfsh
 import (
 	"github.com/juanjux/python-driver/driver/normalizer/pyast"
-	"github.com/bblfsh/sdk/uast"
+	. "github.com/bblfsh/sdk/uast"
+	. "github.com/bblfsh/sdk/uast/ann"
 )
 
 /*
-A lot of stuff is currently missing from the generated UAST. See:
+Some stuff is missing from the current UAST spec to fully represent a Python AST. Issue:
 
 https://github.com/bblfsh/documentation/issues/13
 
@@ -52,76 +54,75 @@ https://greentreesnakes.readthedocs.io/en/latest/nodes.html?highlight=joinedstr#
 	// Other:
 	Starred          => *expanded_list, could be translated to UnaryOp.Star
  */
+var AnnotationRules = On(Any).Self(
+	On(Not(HasInternalType(pyast.Module))).Error("root must be Module"),
+	On(HasInternalType(pyast.Module)).Roles(File).Descendants(
+		On(HasInternalType(pyast.Module)).Roles(PackageDeclaration),
 
-// AnnotationRules for Python UAST.
-var AnnotationRules uast.Rule = uast.Rules(
-	uast.OnInternalType(pyast.Module).Role(uast.File),
-	// FIXME: check how to add annotations and add them
-	uast.OnInternalType(pyast.Name).Role(uast.SimpleIdentifier),
-	uast.OnInternalType(pyast.Expression).Role(uast.File),
-	uast.OnInternalType(pyast.Expr).Role(uast.File),
-	uast.OnInternalType(pyast.expr).Role(uast.File),
-	uast.OnInternalType(pyast.Assert).Role(uast.Assert),
+		On(HasInternalType(pyast.Module)).Roles(File),
+		// FIXME: check how to add annotations and add them
+		On(HasInternalType(pyast.Name)).Roles(SimpleIdentifier),
+		On(HasInternalType(pyast.Expression)).Roles(File),
+		On(HasInternalType(pyast.Expr)).Roles(File),
+		On(HasInternalType(pyast.expr)).Roles(File),
+		On(HasInternalType(pyast.Assert)).Roles(Assert),
 
-	uast.OnInternalType(pyast.Constant).Role(uast.Literal),
-	uast.OnInternalType(pyast.StringLiteral).Role(uast.StringLiteral),
-	// FIXME: should we make a distinction between StringLiteral and ByteLiteral on the UAST?
-	uast.OnInternalType(pyast.ByteLiteral).Role(uast.StringLiteral),
-	// FIXME: JoinedStr are the fstrings (f"my name is {name}"), they have a composite AST
-	// with a body that is a list of StringLiteral + FormattedValue(value, conversion, format_spec)
-	uast.OnInternalType(pyast.JoinedStr).Role(uast.StringLiteral),
-	uast.OnInternalType(pyast.NoneLiteral).Role(uast.NullLiteral),
-	uast.OnInternalType(pyast.NumLiteral).Role(uast.NumberLiteral),
-	// FIXME: change these to ContainerLiteral/CompoundLiteral/whatever if they're added
-	uast.OnInternalType(pyast.Set).Role(uast.Literal),
-	uast.OnInternalType(pyast.List).Role(uast.Literal),
-	uast.OnInternalType(pyast.Dict).Role(uast.Literal),
-	uast.OnInternalType(pyast.Tuple).Role(uast.Literal),
-	uast.OnInternalType(pyast.Try).Role(uast.Try),
-	// FIXME: add OnPath Try.body (uast_type=ExceptHandler) => TryBody
-	uast.OnInternalType(pyast.TryExcept).Role(uast.TryCatch),
-	uast.OnInternalType(pyast.TryFinally).Role(uast.TryFinally),
-	uast.OnInternalType(pyast.Raise).Role(uast.Throw),
-	// FIXME: review, add path for the body and items childs
-	// FIXME: withitem on Python to RAII on a resource and can aditionally create and alias on it,
-	// both of which currently doesn't have representation in the UAST
-	uast.OnInternalType(pyast.With).Role(uast.BlockScope),
-	uast.OnInternalType(pyast.Return).Role(uast.Return),
-	uast.OnInternalType(pyast.Break).Role(uast.Break),
-	uast.OnInternalType(pyast.Continue).Role(uast.Continue),
-	// FIXME: extract the test, orelse and the body to test-> IfCondition, orelse -> IfElse, body -> IfBody
-	// UAST are first level members
-	uast.OnInternalType(pyast.If).Role(uast.If),
-	// One liner if, like a normal If but it will be inside an Assign (like the ternary if in C)
-	// also applies the comment about the If
-	uast.OnInternalType(pyast.IfExp).Role(uast.If),
-	// FIXME: Import and ImportFrom can make an alias (name -> asname), extract it and put it as
-	// uast.ImportAlias
-	uast.OnInternalType(pyast.Import).Role(uast.Import),
-	uast.OnInternalType(pyast.ImportFrom).Role(uast.Import),
-	uast.OnInternalType(pyast.ClassDef).Role(uast.TypeDeclaration),
-	// FIXME: add .args[].arg, .body, .name, .decorator_list[]
-	uast.OnInternalType(pyast.FunctionDef).Role(uast.FunctionDeclaration),
-	// FIXME: Internal keys for the ForEach: iter -> ?, target -> ?, body -> ForBody,
-	uast.OnInternalType(pyast.For).Role(uast.ForEach),
-	// FIXME: while internal keys: body -> WhileBody, orelse -> ?, test -> WhileCondition
-	uast.OnInternalType(pyast.While).Role(uast.While),
-	// FIXME: detect qualified 'Call.func' with a "Call.func.value" member and
-	// "Call.func.ast_type" == attr (module/object calls) and convert the to this UAST:
-	// MethodInvocation + MethodInvocationObject (func.value.id) + MethodInvocationName (func.attr)
-	uast.OnInternalType(pyast.Pass).Role(uast.Noop),
-	uast.OnInternalType(pyast.Str).Role(uast.StringLiteral),
-	uast.OnInternalType(pyast.Num).Role(uast.NumberLiteral),
-	uast.OnInternalType(pyast.Assign).Role(uast.Assignment),
-	// FIXME: this is the annotated assignment (a: annotation = 3) not exactly Assignment
-	// it also lacks AssignmentValue and AssignmentVariable (see how to add them)
-	uast.OnInternalType(pyast.AnnAssign).Role(uast.Assignment),
-	// FIXME: this is the a += 1 style assigment
-	uast.OnInternalType(pyast.AugAssign).Role(uast.Assignment),
+		On(HasInternalType(pyast.Constant)).Roles(Literal),
+		On(HasInternalType(pyast.StringLiteral)).Roles(StringLiteral),
+		// FIXME: should we make a distinction between StringLiteral and ByteLiteral on the UAST?
+		On(HasInternalType(pyast.ByteLiteral)).Roles(StringLiteral),
+		// FIXME: JoinedStr are the fstrings (f"my name is {name}"), they have a composite AST
+		// with a body that is a list of StringLiteral + FormattedValue(value, conversion, format_spec)
+		On(HasInternalType(pyast.JoinedStr)).Roles(StringLiteral),
+		On(HasInternalType(pyast.NoneLiteral)).Roles(NullLiteral),
+		On(HasInternalType(pyast.NumLiteral)).Roles(NumberLiteral),
+		// FIXME: change these to ContainerLiteral/CompoundLiteral/whatever if they're added
+		On(HasInternalType(pyast.Set)).Roles(Literal),
+		On(HasInternalType(pyast.List)).Roles(Literal),
+		On(HasInternalType(pyast.Dict)).Roles(Literal),
+		On(HasInternalType(pyast.Tuple)).Roles(Literal),
+		On(HasInternalType(pyast.Try)).Roles(Try),
+		// FIXME: add OnPath Try.body (uast_type=ExceptHandler) => TryBody
+		On(HasInternalType(pyast.TryExcept)).Roles(TryCatch),
+		On(HasInternalType(pyast.TryFinally)).Roles(TryFinally),
+		On(HasInternalType(pyast.Raise)).Roles(Throw),
+		// FIXME: review, add path for the body and items childs
+		// FIXME: withitem on Python to RAII on a resource and can aditionally create and alias on it,
+		// both of which currently doesn't have representation in the UAST
+		On(HasInternalType(pyast.With)).Roles(BlockScope),
+		On(HasInternalType(pyast.Return)).Roles(Return),
+		On(HasInternalType(pyast.Break)).Roles(Break),
+		On(HasInternalType(pyast.Continue)).Roles(Continue),
+		// FIXME: extract the test, orelse and the body to test-> IfCondition, orelse -> IfElse, body -> IfBody
+		// UAST are first level members
+		On(HasInternalType(pyast.If)).Roles(If),
+		// One liner if, like a normal If but it will be inside an Assign (like the ternary if in C)
+		// also applies the comment about the If
+		On(HasInternalType(pyast.IfExp)).Roles(If),
+		// FIXME: Import and ImportFrom can make an alias (name -> asname), extract it and put it as
+		// uast.ImportAlias
+		On(HasInternalType(pyast.Import)).Roles(Import),
+		On(HasInternalType(pyast.ImportFrom)).Roles(Import),
+		On(HasInternalType(pyast.ClassDef)).Roles(TypeDeclaration),
+		// FIXME: add .args[].arg, .body, .name, .decorator_list[]
+		On(HasInternalType(pyast.FunctionDef)).Roles(FunctionDeclaration),
+		// FIXME: Internal keys for the ForEach: iter -> ?, target -> ?, body -> ForBody,
+		On(HasInternalType(pyast.For)).Roles(ForEach),
+		// FIXME: while internal keys: body -> WhileBody, orelse -> ?, test -> WhileCondition
+		On(HasInternalType(pyast.While)).Roles(While),
+		// FIXME: detect qualified 'Call.func' with a "Call.func.value" member and
+		// "Call.func.ast_type" == attr (module/object calls) and convert the to this UAST:
+		// MethodInvocation + MethodInvocationObject (func.value.id) + MethodInvocationName (func.attr)
+		On(HasInternalType(pyast.Pass)).Roles(Noop),
+		On(HasInternalType(pyast.Str)).Roles(StringLiteral),
+		On(HasInternalType(pyast.Num)).Roles(NumberLiteral),
+		On(HasInternalType(pyast.Assign)).Roles(Assignment),
+		// FIXME: this is the annotated assignment (a: annotation = 3) not exactly Assignment
+		// it also lacks AssignmentValue and AssignmentVariable (see how to add them)
+		On(HasInternalType(pyast.AnnAssign)).Roles(Assignment),
+		// FIXME: this is the a += 1 style assigment
+		On(HasInternalType(pyast.AugAssign)).Roles(Assignment),
+	),
 )
 
-// Annotate annotates the given Java UAST.
-func Annotate(n *uast.Node) error {
-	return uast.PreOrderVisit(n, AnnotationRules)
-}
 
