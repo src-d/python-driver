0459d03ed335fa75bbf55d44c049b77b8c921cc1
Juanjo Alvarez <juanjux@gmail.com>
2017-03-28 10:51:01 +0200

    Added a new integration tests for def parameters and modified the with one
    Bumped pydetector requirement for a bugfix

--- native/python_package/setup.py
+++ native/python_package/setup.py
@@ -34,7 +34,7 @@ setup(
     },
     install_requires=[
         "msgpack-python==0.4.8",
-        "pydetector==0.4.3"
+        "pydetector==0.4.4"
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
