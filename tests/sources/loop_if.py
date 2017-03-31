8a79d4219d5c15b9107862fba90bc22e20d70741
Juanjo Alvarez <juanjux@gmail.com>
2017-03-31 09:17:46 +0200

    Use the new UAST comparison operators. Updated integration-tests. Fixed bugs

--- tests/sources/loop_if.py
+++ tests/sources/loop_if.py
@@ -1,14 +1,4 @@
-something = [1,2,3]
-
 for i in something:
-    if stuff:
-        continue
-    elif otherstuff:
-        break
+    pass
 else:
     print("Python fors have elses")
-
-mydict = {1:1, 2:2}
-for k, v in mydict.items():
-    print(k)
-    print(v)
